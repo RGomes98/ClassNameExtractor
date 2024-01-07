0f3ad8e2e50baba12114dd7b9eff263ccb74225b
Renan Gomes <renango2015@gmail.com>
2024-01-07 13:51:16 -0300

    feat: add website new features

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
-import { FormElement, handleFormSubmit } from './elements/FormElement';
-import { handleOptionSelect } from './elements/SelectElement';
-import { handleInputChange } from './elements/InputElement';
+import { handleInputChange } from './components/Form/FormModuleNameInput';
+import { handleOptionSelect } from './components/Form/FormOptions';
+import { Form, handleFormSubmit } from './components/Form/Form';
+import { FooterLink } from './components/Footer/FooterLink';
+import { Heading } from './components/Heading/Heading';
+import { Overlay } from './components/Overlay/Overlay';
 
 import './globals.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <main>
-      ${FormElement()}
+      ${Heading()}
+      ${Form()}
     </main>
+    <footer>
+      ${FooterLink()}
+    </footer>
+    ${Overlay()}
 `;
 
-document.getElementById('formModuleInput')!.addEventListener('change', handleInputChange);
-document.getElementById('formSelect')!.addEventListener('change', handleOptionSelect);
+document.getElementById('form-module-input')!.addEventListener('change', handleInputChange);
+document.getElementById('form-select')!.addEventListener('change', handleOptionSelect);
 document.getElementById('form')!.addEventListener('submit', handleFormSubmit);
