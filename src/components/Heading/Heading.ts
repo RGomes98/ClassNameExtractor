ea7b459efc60f9c733538187ed898537636e1011
Renan Gomes <renango2015@gmail.com>
2024-01-07 13:49:53 -0300

    feat: add website heading

---
+++ src/components/Heading/Heading.ts
@@ -0,0 +1,13 @@
+export const Heading = () => {
+  return `
+    <div class='heading-wrapper'>
+      <h1 class='heading-title'>Code Chisel</h1>
+      <p class='heading-description'>
+        Welcome to CodeChisel – your digital tool for extracting class names with precision! Streamline your
+        workflow and effortlessly carve out clean and organized styles from code snippets. Whether it's HTML,
+        JSX, or JSX with CSS modules, simply input your code, select the language, and let Code Chisel do the
+        rest. Elevate your development productivity with this intuitive class extraction tool.
+      </p>
+    </div>
+  `;
+};
