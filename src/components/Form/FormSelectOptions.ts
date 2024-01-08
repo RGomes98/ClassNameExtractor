833b4fbfb525e040a94020b9d99cf24679ab84a1
Renan Gomes <renango2015@gmail.com>
2024-01-08 10:26:50 -0300

    style: change form-select id order

--- src/components/Form/FormSelectOptions.ts
+++ src/components/Form/FormSelectOptions.ts
@@ -2,7 +2,7 @@ export const FormSelectOptions = () => {
   return `
     <div id='format-wrapper' class='format-wrapper'>
       <label class='label' for='form-select'>Select code format:</label>
-      <select class='select-options' id='form-select'>
+      <select id='form-select' class='select-options'>
           <option class='option' value='jsx'>JSX</option>
           <option class='option' value='html'>HTML</option>
           <option class='option' value='module'>JSX with CSS Modules</option>
